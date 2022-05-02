aff9a958e8f05c687995a6016adf7753ded2f5ad
ACEslava <aeslava1970@gmail.com>
2022-05-01 19:22:43 -0700

    Suggested Changes for PR#5

--- bot/cogs/custom_ui/page_turn_embed.py
+++ bot/cogs/custom_ui/page_turn_embed.py
@@ -7,14 +7,16 @@ from discord.ext import commands
 class PageTurnView(discord.ui.View):
     """discord.ui.View subclass for page-turn functionality between embeds
 
-    :param ctx: Discord's command context
-    :type ctx: discord.ext.commands.Context
+    Parameters
+    ----------
+    ctx : discord.ext.commands.Context
+        Discord's command context
 
-    :param embed_list: List of embeds to page turn
-    :type embed_list: List[discord.Embed]
+    embed_list: List[discord.Embed]
+        List of embeds to page turn
 
-    :param timeout: (Optional) Float indicating seconds before timeout. (Default=60.0)
-    :type timeout: float
+    timeout: float
+        (Optional) Float indicating seconds before timeout. (Default=60.0)
     """
 
     def __init__(
@@ -26,23 +28,22 @@ class PageTurnView(discord.ui.View):
 
         super().__init__(timeout=timeout)
 
-    # Checks that the user who invoked the command is the one interacting
     async def interaction_check(self, interaction: discord.Interaction) -> bool:
         return interaction.user.id == self.ctx.message.author.id
 
-    # Remove buttons on timeout (idk if it works, haven't tested)
+    # Remove buttons on timeout
     async def on_timeout(self) -> None:
         super().clear_items()
         super().stop()
 
     # Delete result
     @discord.ui.button(emoji="🗑️", style=discord.ButtonStyle.red)
-    async def delete_callback(self, interaction, button):
+    async def delete_callback(self, interaction: discord.Interaction):
         await interaction.message.delete()
 
     # Prev page
     @discord.ui.button(emoji="◀️", style=discord.ButtonStyle.gray)
-    async def prev_callback(self, interaction, button):
+    async def prev_callback(self, interaction: discord.Interaction):
         self.current_page -= 1
         await interaction.response.edit_message(
             content="",
@@ -51,7 +52,7 @@ class PageTurnView(discord.ui.View):
 
     # Next page
     @discord.ui.button(emoji="▶️", style=discord.ButtonStyle.gray)
-    async def next_callback(self, interaction: discord.Interaction, button):
+    async def next_callback(self, interaction: discord.Interaction):
         self.current_page += 1
         await interaction.response.edit_message(
             content="",
